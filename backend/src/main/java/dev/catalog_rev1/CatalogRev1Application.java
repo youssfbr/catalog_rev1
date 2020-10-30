c4b7453dd7e77776ca1e4e8cd734a610dc373507
alisson youssf <youssfbr@gmail.com>
2020-10-30 14:06:16 -0300

    Project created

---
+++ backend/src/main/java/dev/catalog_rev1/CatalogRev1Application.java
@@ -0,0 +1,13 @@
+package dev.catalog_rev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogRev1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogRev1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/catalog_rev1/CatalogRev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.catalog_rev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogRev1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
