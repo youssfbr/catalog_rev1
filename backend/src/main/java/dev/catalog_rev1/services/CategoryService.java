2a177ee9d70c7e9b4b4b043e9ce97389ba980c14
alisson youssf <youssfbr@gmail.com>
2020-10-31 13:55:01 -0300

    @Transactional

--- backend/src/main/java/dev/catalog_rev1/services/CategoryService.java
+++ backend/src/main/java/dev/catalog_rev1/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.catalog_rev1.entities.Category;
 import dev.catalog_rev1.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
