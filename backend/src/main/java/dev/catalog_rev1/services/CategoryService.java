139044d1d49d572cffc5ac18f0b1771db9a256d9
alisson youssf <youssfbr@gmail.com>
2020-11-02 13:37:25 -0300

    POST Category

--- backend/src/main/java/dev/catalog_rev1/services/CategoryService.java
+++ backend/src/main/java/dev/catalog_rev1/services/CategoryService.java
@@ -31,4 +31,13 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new NotFoundException("Category Id not found"));		
 		return new CategoryDTO(entity);
 	}
+	
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}		
+	
 }
--- backend/src/main/java/dev/catalog_rev1/services/exceptions/NotFoundException.java
+++ backend/src/main/java/dev/catalog_rev1/services/exceptions/NotFoundException.java
@@ -1,6 +1,6 @@
 package dev.catalog_rev1.services.exceptions;
 
-public class NotFoundException extends RuntimeException{	
+public class NotFoundException extends RuntimeException {	
 	private static final long serialVersionUID = 1L;
 
 	public NotFoundException(String msg) {
