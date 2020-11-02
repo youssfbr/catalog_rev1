839e7561ee3f71759603676038a7946b2e05625a
alisson youssf <youssfbr@gmail.com>
2020-11-02 19:57:13 -0300

    PUT Category

--- backend/src/main/java/dev/catalog_rev1/services/CategoryService.java
+++ backend/src/main/java/dev/catalog_rev1/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,6 +40,19 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);	
+		} 
+		catch (EntityNotFoundException e) {
+			throw new NotFoundException("Category Id " + id + " not found");
+		}		
 	}		
 	
 }
